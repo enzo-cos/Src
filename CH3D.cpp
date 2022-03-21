6d89f0002b7af3f23a0e036ffee6cd910951a7ba
ecostant <enzo.costantini2@gmail.com>
2022-03-21 12:22:10 +0100

    MAJ

--- CH3D.cpp
+++ CH3D.cpp
@@ -1,18 +1,19 @@
 #include "CH3D.h"
-	CH3D::CH3D(void) {
-		x = y = z = w = 0.0F;
-	}
-	CH3D::CH3D(float _x, float _y, float _z, float _w) {
-		x = _x;
-		y = _y;
-		z = _z;
-		w = _w;
-	}
-	CH3D::CH3D(CH3D* ch) {
-		x = ch->x;
-		y = ch->y;
-		z = ch->z;
-		w = ch->w;
-	}
-	CH3D::~CH3D(void){}
+
+CH3D::CH3D(void) {
+	x = y = z = w = 0.0F;
+}
+CH3D::CH3D(float _x, float _y, float _z, float _w) {
+	x = _x;
+	y = _y;
+	z = _z;
+	w = _w;
+}
+CH3D::CH3D(CH3D* ch) {
+	x = ch->x;
+	y = ch->y;
+	z = ch->z;
+	w = ch->w;
+}
+CH3D::~CH3D(void){}
 	

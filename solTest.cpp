217f852fdbb0f38f6b143183e6d50f64601545ff
ecostant <enzo.costantini2@gmail.com>
2022-04-12 23:47:05 +0200

    Ajout test caméra

--- solTest.cpp
+++ solTest.cpp
@@ -5,7 +5,9 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <vector>
 #include "PNG/ChargePngFile.h"
+#include "Dir3D.h"
 
 /* Variables globales                           */
 
@@ -17,21 +19,51 @@ static int wPy = 50;               // Position verticale de la fenetre
 static GLenum face = GL_FRONT_AND_BACK;
 static GLenum mode = GL_FILL;
 static bool anim = false;
+static bool cam = false;
 static float rota = 0.0F;
 static float turn = 0.0F;
 static float nbTour = 0.0F;
+
 
 static float c1 = 1.0F;
 static float c2 = 1.0F;
 static float c3 = 1.0F;
 
-static unsigned int textureID = 0;
-static int texture = 1;
+static float avX = 0.0F;
+static float avY = 3.0F;
+static float avZ = 0.0F;
+
+static double posX = 0.0;
+static double posY = 10.0;
+static double posZ = 80.0;
+
+static double posRobotX = 0.0;
+static double posRobotY = 10.0;
+static double posRobotZ = 0.0;
+
+static double dirX=0.0;
+static double dirY=0.0;
+static double dirZ=10.0;
+
+static int rotaR = 0;
+static double prop = 10.0;
+static double toRad = 3.14159 / 180.0;
+
+static int mouseX = 0.0;
+static int mouseY = 0.0;
+
+static float position1[4] = { 100.0F,0.0F,0.0F,0.0F };
+static float position2[4] = { 0.0F,0.0F,100.0F,0.0F };
+static float diffuse[4] = { 1.0F,1.0F,1.0F,1.0F };
+static float ambiant[4] = { 0.2F,0.2F,0.2F,1.0F };
+static float spec[4] = { 1.0F,1.0F,1.0F,1.0F };
+static float tJaune[4] = { 1.0F,1.0F,1.0F,1.0F };
 
 /* Fonction d'initialisation des parametres     */
 /* OpenGL ne changeant pas au cours de la vie   */
 /* du programme                                 */
-
+static unsigned int textureID = 0;
+static int texture =0;
 
 static void initTexture(void) {
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -63,6 +95,8 @@ static void initTexture(void) {
 static void init(void) {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    //glEnable(GL_LIGHT1);
+    //glEnable(GL_LIGHT2);
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_NORMALIZE);
@@ -72,118 +106,114 @@ static void init(void) {
 
 /** cube trou  */
 static void cubeFacette() {
-    initTexture();
+    
     //face
     glNormal3f(0.0F, 0.0F, 1.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(-0.5F, 0.5F,0.5F); //1
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(-0.5F, -0.5F,0.5F); //2 
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(0.5F, -0.5F, 0.5F); //3 
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(0.5F, 0.5F, 0.5F); //4
     glEnd();
     //droite
     glNormal3f(1.0F, 0.0F, 0.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(0.5F, 0.5F, 0.5F); //4
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(0.5F, -0.5F, 0.5F); //3
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(0.5F, -0.5F, -0.5F); //7
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(0.5F, 0.5F, -0.5F); //8
     glEnd();
     //derriere
     glNormal3f(0.0F, 0.0F, -1.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(0.5F, 0.5F, -0.5F); //8
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(0.5F, -0.5F, -0.5F); //7
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(-0.5F, -0.5F, -0.5F); //6
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(-0.5F, 0.5F, -0.5F); //5
     glEnd();
     //bas
     glNormal3f(0.0F, -1.0F, 0.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(0.5F, -0.5F, 0.5F); //3 
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(-0.5F, -0.5F, 0.5F); //2 
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(-0.5F, -0.5F, -0.5F); //6
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(0.5F, -0.5F, -0.5F); //7
     glEnd();
     //gauche
     glNormal3f(-1.0F, 0.0F, 0.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(-0.5F, -0.5F, 0.5F); //2 
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(-0.5F, 0.5F, 0.5F); //1
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(-0.5F, 0.5F, -0.5F); //5
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(-0.5F, -0.5F, -0.5F); //6
     glEnd();
     //haut
     glNormal3f(0.0F, 1.0F, 0.0F);
     glBegin(GL_QUADS);
-    glTexCoord2f(0.0F, 1.0F);
     glVertex3f(-0.5F, 0.5F, 0.5F); //1
-    glTexCoord2f(1.0F, 1.0F);
     glVertex3f(0.5F, 0.5F, 0.5F); //4
-    glTexCoord2f(1.0F, 0.0F);
     glVertex3f(0.5F, 0.5F, -0.5F); //8
-    glTexCoord2f(0.0F, 0.0F);
     glVertex3f(-0.5F, 0.5F, -0.5F); //5
     glEnd();
 
 }
 
-/** carre facette trouée   */
-static void carreFacette() {
+static void genTerrain() {
+    glPushMatrix();
+    float y = 0.0F;
+    float x=-50.0F;
+    float z = -50.0F;
+    float c = 2.0f;
+    //bool dec = false;
+    //    while (x < 55.0) {
+    //        z = -50.0f;
+    //        while (z < 53.0) {
+
+    //            glNormal3f(0.0F, 1.0F, 0.0F);
+    //            glBegin(GL_QUADS);
+    //            glTexCoord2f(0.0F, 0.0F);
+    //            glVertex3f(x, y, z);
+    //            //glTexCoord2f(1.0F, 0.0F);
+    //            glVertex3f(x + c, y, z);
+    //            //Modif y pour relief
+    //            //glTexCoord2f(1.0F, 1.0F);
+    //            glVertex3f(x + c, y, z + c);
+    //            //glTexCoord2f(0.0F, 1.0F);
+    //            glVertex3f(x, y, z + c);
+    //            glEnd();
+    //            z += c;
+    //        }
+    //        x += c;
+    //    }
+    glNormal3f(0.0F, 1.0F, 0.0F);
+    glBegin(GL_QUADS);
+   // glTexCoord2f(0.0F, 1.0F);
+    glVertex3f(-50, y, 50);
+    //glTexCoord2f(1.0F, 1.0F);
+    glVertex3f(50, y, 50);
+   // glTexCoord2f(1.0F, 0.0F);
+    glVertex3f(50, y, -50);   
+    //glTexCoord2f(0.0F, 0.0F);
+    glVertex3f(-50, y, -50);
     
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(1.0F, -1.0F); //1
-    glVertex2f(0.0F, -0.5F); //2
-
-    glVertex2f(-1.0F, -1.0F); //3
-    glVertex2f(-0.5F, 0.0F); //4
-    glVertex2f(-1.0F, 1.0F); //5 
-    glVertex2f(0.0F, 0.5F); //6
-    glVertex2f(1.0F, 1.0F); //7 
-    glVertex2f(0.5F, 0.0F); //8
-
-    glVertex2f(1.0F, -1.0F); //9 
-    glVertex2f(0.0F, -0.5F); //10
-
     glEnd();
+    glPopMatrix();
 }
 
-static void cubeTroue() {
-    glNormal3f(0.0F, 0.0F, 1.0F);
-    carreFacette(); //bas 
-    glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-    glTranslatef(0.0F, 0.0F, 1.0F);
-    
-    glNormal3f(1.0F, 0.0F, 0.0F);
-    carreFacette(); //droite 
+static void DeplSph(void) {
+    glPushMatrix();
+    glTranslatef(avX, avY, avZ);
+    glutSolidSphere(3.5, 250, 250);
+    glTranslatef(avX+10, avY, avZ+5);
+    glutSolidSphere(2.5, 150, 150);
+    glPopMatrix();
 }
-
 /* Scene dessinee  */
 
 static void scene(void) {
     glPushMatrix();
-    cubeFacette();
-    //cubeTroue();
+    genTerrain();
+    DeplSph();
     glPopMatrix();
 }
 
@@ -196,13 +226,33 @@ static void display(void) {
     else
         glDisable(GL_TEXTURE_2D);
     printf("D\n");
+    /*rota = 25.0;
+    turn = 10.0;*/
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPolygonMode(face, mode);
+    glLightfv(GL_LIGHT1, GL_POSITION, position1);
+    glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 180.0F);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambiant);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, spec);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, tJaune);
+
+    glLightfv(GL_LIGHT2, GL_POSITION, position2);
+    glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 180.0F);
+    glLightfv(GL_LIGHT2, GL_AMBIENT, ambiant);
+    glLightfv(GL_LIGHT2, GL_SPECULAR, spec);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, tJaune);
+
+   
     float t[4] = { c1,c2,c3,1.0F };
     glMaterialfv(GL_FRONT, GL_DIFFUSE, t);
     glPushMatrix(); 
-        glRotatef(rota, 1.0F, 0.0F, 0.0F);
-        glRotatef(turn, 0.0F, 1.0F, 0.0F);
+    if (!cam) {
+        gluLookAt(posX, posY, posZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    }
+    else {
+       gluLookAt(posRobotX, posRobotY, posRobotZ, posRobotX + dirX, 0.0, posRobotZ+dirZ, 0.0, 1.0, 0.0);
+    }
+        
     scene();
     glPopMatrix();
     glFlush();
@@ -222,7 +272,27 @@ static void reshape(int wx, int wy) {
     glViewport(0, 0, wx, wy);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    double ratioP = (double)wx / (double)wy;
+    if (!cam) {
+        
+
+        double posModif = (posZ) * -1;
+        //multA = -100 / posZ;
+        printf("pos = %f\n", posModif);
+        /*if (ratioP > 1) {
+            gluPerspective(45.0 * 1.0, ratioP, posZ-40, posZ-120);
+        }
+        else {
+            gluPerspective(45.0 * 1.0/ratioP, ratioP, posZ - 40, posZ - 120);
+        }*/
+        //glOrtho(posX-30, posX+30, posY-20, posY+20, posZ-100, posZ);
+        gluPerspective(45.0 * 1.0 / ratioP, ratioP, posZ - 40, posZ - 120);
+    }
+    else {
+        gluPerspective(90, ratioP, -60.0, 0.0);
+        //glOrtho(-51.0, 51.0, -51.0, 51.0, -51.0, 51.0);
+    }
+    
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -254,7 +324,6 @@ static void keyboard(unsigned char key, int x, int y) {
             mode = GL_FILL;
             break;
         }
-        texture = (texture + 1) % 2;
         glutPostRedisplay();
         break;
 
@@ -297,6 +366,42 @@ static void keyboard(unsigned char key, int x, int y) {
         if (c2 < 1) c3 += 0.1F;
         glutPostRedisplay();
         break;
+
+    case 'z':
+        posZ -= 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 'q':
+        posX -= 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 's':
+        posZ += 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 'd':
+        posX += 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 'y':
+        posY += 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 'Y':
+        posY -= 1.0;
+        glutPostRedisplay();
+        break;
+
+    case 'c':
+        cam = !cam;
+        glutPostRedisplay();
+        break;
+
     case 0x1B://echap
         exit(0);
         break;
@@ -312,22 +417,31 @@ static void special(int specialKey, int x, int y) {
     printf("S  %4d %4d %4d\n", specialKey, x, y);
     switch (specialKey) {
         case 100: //fleche gauche
-            turn -= 2.0F;
+            avZ -= 1.0F;
+            posRobotZ--;
+            //turn--;
             glutPostRedisplay();
             break;
 
         case 101: //fleche haut
-            rota -= 2.0F;
+            
+            avX += 1.0F;
+            posRobotX++;
+            //rota++;
             glutPostRedisplay();
             break;
 
         case 102: //fleche droite
-            turn += 2.0F;
+            avZ += 1.0F;
+            posRobotZ++;
+            //turn++;
             glutPostRedisplay();
             break;
 
         case 103: //fleche bas
-            rota += 2.0F;
+            avX -= 1.0F;
+            posRobotX--;
+            //rota--;
             glutPostRedisplay();
             break;
     }
@@ -338,6 +452,8 @@ static void special(int specialKey, int x, int y) {
 
 static void mouse(int button, int state, int x, int y) {
     printf("M  %4d %4d %4d %4d\n", button, state, x, y);
+    mouseX = x;
+    mouseY = y;
 }
 
 /* Fonction executee lors du passage            */
@@ -346,6 +462,13 @@ static void mouse(int button, int state, int x, int y) {
 
 static void mouseMotion(int x, int y) {
     printf("MM %4d %4d\n", x, y);
+    int diffX = mouseX - x;
+    int diffY = mouseY - y;
+    rotaR = (rotaR % 360) - (diffX / 50);
+    dirZ = cos(rotaR * toRad) * prop;
+    dirX = sin(rotaR * toRad) * prop;
+    posZ += (-diffY / 50);
+    glutPostRedisplay();
 }
 
 /* Fonction executee lors du passage            */
@@ -360,8 +483,7 @@ static void passiveMouseMotion(int x, int y) {
 /* lors de l'exécution de la fonction exit()    */
 
 static void clean(void) {
-    if (textureID != 0)
-        glDeleteTextures(1, &textureID);
+    printf("Bye\n");
 }
 
 /* Fonction principale                          */
